80ef65c301099907b0df6fe244edf0dfefd15e3b
rfdolan <rfdolan@wpi.edu>
2019-12-14 23:36:53 -0500

    uncomment

--- src/problem12/Main.java
+++ src/problem12/Main.java
@@ -263,7 +263,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//System.out.println(problem12part1());
+		System.out.println(problem12part1());
 		System.out.println(problem12part2());
 
 	}
